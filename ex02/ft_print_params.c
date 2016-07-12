5a565cc5d88da195c448cde15e8a9fd8928b7fa1
Brice WAEGENEIRE <bricewge@gmail.com>
2016-07-13 00:30:36 +0200

    ex02

---
+++ ex02/ft_print_params.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bwaegene <brice.wge@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/07/13 00:19:40 by bwaegene          #+#    #+#             */
+/*   Updated: 2016/07/13 00:30:24 by bwaegene         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	if (argc >= 0)
+	{
+		while (argv[i])
+		{
+			ft_putstr(argv[i]);
+			ft_putchar('\n');
+			i++;
+		}
+	}
+	return (0);
+}
